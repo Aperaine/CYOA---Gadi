79d9058746a908bc0e84e27e095b1eccf999c64f
Aperaine <Aperaine@users.noreply.github.com>
2025-10-21 16:02:51 +0900

    Animated typing

--- first trial.py
+++ first trial.py
@@ -6,6 +6,22 @@ hasStick:bool = False
 
 
 #functions
+def typeWrite(text:str, speed:int = 1):
+    firstChar = True
+    for letter in text:
+        if letter == "\n" and not firstChar:
+            time.sleep(1 * speed)
+            print("")
+            continue
+        print(letter, end="")
+        if letter == ",":
+            time.sleep(0.5 * speed)
+        else:
+            time.sleep(0.02 * speed)
+        
+        firstChar = False
+    print("")
+
 def is_convertible_to_int(value):
     try:
         int(value)
@@ -23,8 +39,9 @@ def Response(question, *answers):
         
         constructedquestion += "\n" + str(iteration) + ": " + str(i)
         iteration += 1
-    
-    response = input(constructedquestion + "\n").lower()
+    typeWrite(constructedquestion, 0.5)
+    print("")
+    response = input().lower()
     if is_convertible_to_int(response):
         if int(response) - 1 in range(len(answers)):
             return int(response)
@@ -33,32 +50,33 @@ def Response(question, *answers):
     for i in range((len(answers) - 1)):
         error += " or " + str(i + 2)
 
-    print(error + ".")
+    typeWrite(error + ".")
     time.sleep(0.5)
     return Response(question, *answers)
 
 def startGame():
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-    print("Temple Walk, a Choose-Your-Own-Adventure game")
+    typeWrite("Temple Walk, a Choose-Your-Own-Adventure game", 2)
     time.sleep(2)
-    print("\nYou find yourself in a stone path.\nTwo illuminated torii shine bright-red in front of you.")
-    time.sleep(2)
-    print("Seeing the dimly-lit temple ahead, you proceed forward.\nAs you walk, you see a wooden stick to your right.")
+    print("")
+    typeWrite("You find yourself in a stone path.\nTwo illuminated torii shine bright-red in front of you.")
+    time.sleep(1)
+    typeWrite("Seeing the dimly-lit temple ahead, you proceed forward.\nAs you walk, you see a wooden stick to your right.")
     time.sleep(2)
     match Response("Take it with you?", "Yes, Hold it in your hand.", "Yes, Put it in your pocket.", "No, leave it be.", "No, throw it."):
         case 1:
-            print("You picked up the stick. It has a green branch sticking out of it.")
+            typeWrite("You picked up the stick. It has a green branch sticking out of it.")
             hasStick = True
         case 2:
-            print("You picked up the stick and put it in your pocket. It is almost completely sticking out, but it stays put.")
+            typeWrite("You picked up the stick and put it in your pocket. It is almost completely sticking out, but it stays put.")
             hasStick = True
         case 3:
-            print("You ignored the stick")
+            typeWrite("You ignored the stick")
         case 4:
-            print("You threw the stick, it made a satisfying sound.")
+            typeWrite("You threw the stick, it made a satisfying sound.")
             
-    time.sleep(2)
-    print("Continuing ahead, you reach the temple.")
+    time.sleep(1)
+    typeWrite("Continuing ahead, you reach the temple.")
     time.sleep(1.5)
 
 #Ready
